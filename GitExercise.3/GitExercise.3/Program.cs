3289a4c0bf243d3b3f253b2d36b68b27e6bdbd83
Dainichi <dainichi@me.com>
2015-06-19 14:25:46 -0400

    WhUuuuut Up! 1st commit of this exercise

---
+++ GitExercise.3/GitExercise.3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitExercise._3
+{
+    class Program
+    {
+        public static bool isExerciseComplete = false;
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static bool ExpressYoSelf()
+        {
+            Console.WriteLine("..Heading to Florida no time to chat gottta bust out this exercise");
+            //todo come up with better dailoy program string maipulation, bobs example programs? Homework Solutions...
+
+            isExerciseComplete = true;
+            return isExerciseComplete;
+
+        }
+    }
+}
